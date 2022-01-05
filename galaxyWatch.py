e529e701648af13d87feb95414015aff55def17b
Conrad Klaus <conrad.klaus@fokus.fraunhofer.de>
2022-01-05 10:47:39 +0100

    add plots for ski days

---
+++ galaxyWatch.py
@@ -0,0 +1,99 @@
+import datetime
+from typing import List
+import matplotlib.pyplot as plt
+import matplotlib.dates as md
+import os
+from enum import Enum
+
+
+class EntryType(Enum):
+    PULSE = 21
+    ACCELERATION = 1
+    AIRPRESSURE = 6
+    STEPS = 19
+    OK = 231
+
+
+class PlotType(Enum):
+    PULSE = EntryType.PULSE
+    ACCELERATION = EntryType.ACCELERATION
+    AIRPRESSURE = EntryType.AIRPRESSURE
+    STEPS = EntryType.STEPS
+    BATTERY = 2
+
+
+class DataEntry:
+    def __init__(self, time: datetime, battery: int, entry_type: EntryType, sensor_data: int or [int, int, int]):
+        self.time: datetime = time
+        self.battery: int = battery
+        self.type: EntryType = entry_type
+        self.sensor: int or [int, int, int] = sensor_data
+
+    def __lt__(self, other):
+        return self.time < other.time
+
+
+def get_sensor_data():
+    watch_data = []
+    directory = os.fsencode('sensorData')
+    for file in os.listdir(directory):
+        if file.title() == b'Gadgetbridge':
+            continue
+        for line in open('sensorData/' + os.fsdecode(file), 'r').readlines():
+            data = line.replace(',', '.').split()
+            sensor_id = EntryType(int(data[0]))
+            if sensor_id == EntryType.OK:
+                continue
+            timestamp = datetime.datetime.utcfromtimestamp(int(data[1]) / 1000)
+            battery = int(float(data[5]))
+            sensor_data = int(float(data[2]))
+            if sensor_id == EntryType.PULSE and sensor_data == 0:
+                sensor_data = None
+            if sensor_id == EntryType.ACCELERATION:
+                sensor_data = [int(float(data[2])), int(float(data[3])), int(float(data[4]))]
+            watch_data.append(DataEntry(timestamp, battery, sensor_id, sensor_data))
+    watch_data.sort()
+    return watch_data
+
+
+def plot_data(data: List[DataEntry], sensor: PlotType, plot: plt):
+    x: List[datetime] = []
+    y: List[int] = []
+    if sensor == PlotType.ACCELERATION:
+        y: List[int, int, int] = []
+    for i in data:
+        if sensor == PlotType.BATTERY:
+            x.append(i.time)
+            y.append(i.battery)
+            continue
+        if i.type == sensor.value:
+            x.append(i.time)
+            y.append(i.sensor)
+    plot.plot(x, y, label=sensor.name)
+    plot.legend()
+
+
+if __name__ == '__main__':
+   # timeWindow = [datetime.datetime(2020, 1, 1), datetime.datetime(2024, 1, 1)]
+    timeWindow = [datetime.datetime(2022, 1, 1, 6, 30), datetime.datetime(2022, 1, 1, 9)]
+    watch_data = get_sensor_data()
+    watch_data = [i for i in watch_data if timeWindow[0] < i.time < timeWindow[1]]
+    fig, axes = plt.subplots(3, 2, sharex=True, gridspec_kw={'hspace': 0, 'wspace':0})
+    # plt.setp(axa.xaxis.get_majorticklabels(), rotation=45)
+    plt.xticks(rotation=45)
+    fig.suptitle('Galaxy Watch Sensors - morning plus Ski lift')
+    plt.xlabel("time")
+    # plt.xlim([timeWindow[0], timeWindow[1]])
+    plot_data(watch_data, PlotType.PULSE, axes[0, 0])
+    plot_data(watch_data, PlotType.AIRPRESSURE, axes[1, 0])
+    plot_data(watch_data, PlotType.ACCELERATION, axes[2, 0])
+    plot_data(watch_data, PlotType.BATTERY, axes[0, 1])
+    plot_data(watch_data, PlotType.STEPS, axes[1, 1])
+    axes[0, 1].yaxis.tick_right()
+    axes[1, 1].yaxis.tick_right()
+    axes[2, 1].yaxis.tick_right()
+    xfmt = md.DateFormatter('%d - %H:%M')
+    plt.gca().xaxis.set_major_formatter(xfmt)
+    plt.tight_layout()
+    plt.savefig("Galaxy Sensoren Skilift.pdf")
+    plt.show()
---
+++ miBand.py
@@ -0,0 +1,78 @@
+import datetime
+import sqlite3
+import matplotlib.pyplot as plt
+import matplotlib.dates as md
+
+
+timeWindow = [datetime.datetime(2022, 1, 2, 6), datetime.datetime(2022, 1, 2, 18)]
+
+
+conn = sqlite3.connect(r"sensorData/Gadgetbridge")
+cur = conn.cursor()
+cur.execute("SELECT * FROM MI_BAND_ACTIVITY_SAMPLE")
+rows = cur.fetchall()
+
+
+
+class MiBandDataPoint:
+    def __init__(self, time, raw_intensity, steps, raw_kind, heart_rate):
+        if heart_rate == 0 or heart_rate == 255:
+            heart_rate = None
+        self.time = datetime.datetime.utcfromtimestamp(time)
+        self.rawIntensity = raw_intensity
+        self.steps = steps
+        self.rawKind = raw_kind
+        self.heartRate = heart_rate
+
+    def __str__(self):
+        return \
+            "Timestamp: {time}, " \
+            "raw intensity: {rInt}, " \
+            "steps: {steps}, " \
+            "raw kind: {rawKind}, " \
+            "heart rate: {hr}". \
+            format(time=self.time, rInt=self.rawIntensity, steps=self.steps, rawKind=self.rawKind, hr=self.heartRate)
+
+
+datapoints = []
+
+steps = 0
+for row in rows:
+    if not (timeWindow[0] < datetime.datetime.utcfromtimestamp(row[0]) < timeWindow[1]):
+        continue
+    steps += row[4]
+    data = MiBandDataPoint(row[0], row[3], steps, row[5], row[6])
+    datapoints.append(data)
+
+
+plt.title("Mi Band")
+plt.xlabel("Zeit")
+plt.ylabel("Stand [%]")
+plt.xlim([timeWindow[0], timeWindow[1]])
+x = []
+rawKind = []
+steps = []
+heartRate = []
+rawIntensity = []
+for point in datapoints:
+    x.append(point.time)
+    rawKind.append(point.rawKind)
+    steps.append(point.steps)
+    heartRate.append(point.heartRate)
+    rawIntensity.append(point.rawIntensity)
+
+
+fig, ax1 = plt.subplots()
+fig.suptitle('Mi Band - anderer Ski Tag')
+ax2 = ax1.twinx()
+ax1.plot(x, rawKind, label="rawKind")
+ax2.plot(x, steps, label="steps", color="brown")
+ax1.plot(x, heartRate, 'x', label="heartRate")
+ax1.plot(x, rawIntensity, label="rawIntensity")
+plt.xticks(rotation=45)
+xfmt = md.DateFormatter('%d - %H:%M')
+plt.gca().xaxis.set_major_formatter(xfmt)
+fig.legend()
+
+plt.savefig("Mi Band anderer Tag Skilift.pdf")
+plt.show()
